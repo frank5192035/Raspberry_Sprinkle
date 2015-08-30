34e028792c47ec83c3f09a6f06a3729c5b769f0d
root <root@beaglebone.localdomain>
2015-08-30 13:58:16 +0000

    log file function 1st version OK

--- Sprinkle.js
+++ Sprinkle.js
@@ -9,22 +9,23 @@ var request = require("request");
 var b = require('bonescript');
 // }----------------------------------------------------------------------------
 // set Pins {
-var RelayPin = "P9_12";                 // default Relay Pin
-b.pinMode('USR0', b.OUTPUT);            // USR0, USR1 are on/off the same as Relay
-b.pinMode('USR1', b.OUTPUT);
-b.pinMode('USR2', b.OUTPUT);            // USR2 alive toggle with USR3
-b.pinMode('USR3', b.OUTPUT);
-b.pinMode(RelayPin, b.OUTPUT);          // Relay Pin for Turn ON/OFF Motor
-b.digitalWrite(RelayPin, 0);            // turn off motor
-b.digitalWrite('USR0', 0);              // turns the LED OFF
-b.digitalWrite('USR1', 0);
-b.digitalWrite('USR2', 0);
+// var RelayPin = "P9_12";                 // default Relay Pin
+// b.pinMode('USR0', b.OUTPUT);            // USR0, USR1 are on/off the same as Relay
+// b.pinMode('USR1', b.OUTPUT);
+// b.pinMode('USR2', b.OUTPUT);            // USR2 alive toggle with USR3
+// b.pinMode('USR3', b.OUTPUT);
+// b.pinMode(RelayPin, b.OUTPUT);          // Relay Pin for Turn ON/OFF Motor
+// b.digitalWrite(RelayPin, 0);            // turn off motor
+// b.digitalWrite('USR0', 0);              // turns the LED OFF
+// b.digitalWrite('USR1', 0);
+// b.digitalWrite('USR2', 0);
 // }----------------------------------------------------------------------------
 // Global Variables and Constants {
 const minutes = 60000;                  // Minute
 var downCounter = 0;                    // Main Counter of Motor ON
-var intervalObject;                     // Returns a timeoutObject for possible use with clearTimeout()
+// var intervalObject;                     // Returns a timeoutObject for possible use with clearTimeout()
 var accRain = 0.0;                      // Raining accumulation <= 14
+var rainAverage;                        // average of 4 rain station
 var sunriseHour = 6;                    // for comparison of sunrise time
 var sunriseMinute = 0;                  // 
 var sunsetHour = 18;                    // for comparison of sunset time
@@ -33,57 +34,56 @@ var highTemp = 250;                     // Highest Temperature of the day *10
 // }----------------------------------------------------------------------------
 // Initialization {
 setTimeout(checkSchedule, 1);           // Initialization for Main State
-setTimeout(aliveSignal0, 1);            // Initialization for Toggling LED
+// setTimeout(aliveSignal0, 1);            // Initialization for Toggling LED
 setTimeout(crawlKimono, 1);             // Initialization for Kimono network spider
 // }----------------------------------------------------------------------------
 // Initialize the server on port 8168 {
-var server = http.createServer(function (req, res) {
-    var file = '/var/lib/cloud9/BeagleBone_Motor'+((req.url=='/')?'/Grundfos.html':req.url); // requesting files
-    var contentType = 'text/html';
-            // Uncoment if you want to add css to your web page
-            // var path = require('path');
-            // var fileExtension = path.extname(file);
-            // if(fileExtension == '.css') {
-            //     contentType = 'text/css';
-            // }
-    fs.exists(file, function(exists) {
-        if(exists){
-            fs.readFile(file, function(error, content) {
-                if(!error) {
-                    res.writeHead(200,{'content-type':contentType}); // Page found, write content
-                    res.end(content);
-                }
-            })
-        }
-        else {
-            res.writeHead(404);         // Page not found
-            res.end('Page not found');
-        }
-    })
+// var server = http.createServer(function (req, res) {
+//     var file = '/var/lib/cloud9/BeagleBone_Motor'+((req.url=='/')?'/Grundfos.html':req.url); // requesting files
+//     var contentType = 'text/html';
+//             // Uncoment if you want to add css to your web page
+//             // var path = require('path');
+//             // var fileExtension = path.extname(file);
+//             // if(fileExtension == '.css') {
+//             //     contentType = 'text/css';
+//             // }
+//     fs.exists(file, function(exists) {
+//         if(exists){
+//             fs.readFile(file, function(error, content) {
+//                 if(!error) {
+//                     res.writeHead(200,{'content-type':contentType}); // Page found, write content
+//                     res.end(content);
+//                 }
+//             })
+//         }
+//         else {
+//             res.writeHead(404);         // Page not found
+//             res.end('Page not found');
+//         }
+//     })
 // }).listen(8168,'192.168.0.178');
-}).listen(8168);
 // }----------------------------------------------------------------------------
 // socket.io Communication {
-var io = require('socket.io').listen(server); // Loading socket io module
+// var io = require('socket.io').listen(server); // Loading socket io module
 
-io.on('connection', function (socket) { // When communication is established
-    socket.on('pumpON', function(data) {// Clent-size signal for reset downCounter to shower.value
-        var shower = JSON.parse(data);
-        if (shower.on == 1) {
-            downCounter = shower.value; // set or reset downCounter to shower.value
-            logCounter++;
-            console.log(new Date +': Grundfos '+ logCounter +'th turn-on for '+ shower.value +' seconds'); 
-            if (1 == logCounter%2) {    // Two LED for Key Press Toggle
-                b.digitalWrite('USR1', 1);
-                b.digitalWrite('USR2', 0);
-            } else {
-                b.digitalWrite('USR1', 0);
-                b.digitalWrite('USR2', 1);
-            }
-        } else if (shower.on == 0) downCounter = 1;
+// io.on('connection', function (socket) { // When communication is established
+//     socket.on('pumpON', function(data) {// Clent-size signal for reset downCounter to shower.value
+//         var shower = JSON.parse(data);
+//         if (shower.on == 1) {
+//             downCounter = shower.value; // set or reset downCounter to shower.value
+//             logCounter++;
+//             console.log(new Date +': Grundfos '+ logCounter +'th turn-on for '+ shower.value +' seconds'); 
+//             if (1 == logCounter%2) {    // Two LED for Key Press Toggle
+//                 b.digitalWrite('USR1', 1);
+//                 b.digitalWrite('USR2', 0);
+//             } else {
+//                 b.digitalWrite('USR1', 0);
+//                 b.digitalWrite('USR2', 1);
+//             }
+//         } else if (shower.on == 0) downCounter = 1;
         
-    }); // pumpON string from Grundfos.html; shower.on turn on Motor
-});
+//     }); // pumpON string from Grundfos.html; shower.on turn on Motor
+// });
 
 // server.listen(console.log('Grundfos Server is Running: http://' + getIPAddress() + ':8168'));
 // }----------------------------------------------------------------------------
@@ -92,12 +92,11 @@ function crawlKimono() {
     request("https://www.kimonolabs.com/api/6ucbaoiy?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn", 
     function(err, response, body) {     // average rain of pass 24 hours
         var hsinchu = JSON.parse(body);
-        var rainAverage = (parseFloat(hsinchu.results.Rain[0].mm)+
+        rainAverage = (parseFloat(hsinchu.results.Rain[0].mm)+
             parseFloat(hsinchu.results.Rain[1].mm)+parseFloat(hsinchu.results.Rain[2].mm)+
-            parseFloat(hsinchu.results.Rain[3].mm))/4;
+            parseFloat(hsinchu.results.Rain[3].mm))*0.3; // 20% higher than average
         if (rainAverage > 14) rainAverage = 14; // maximum setting for rainAverage = 1 week
         if (rainAverage > accRain) accRain = rainAverage;
-        console.log('rainAverage: '+ rainAverage);
     });
     request("https://www.kimonolabs.com/api/78dba3cq?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn",
     function(err, response, body) {     // Highest Temperature
@@ -108,7 +107,7 @@ function crawlKimono() {
               highTemp = parseInt(str.substring(i+2, str.length))*10; // for down counting second
           }
         }
-        if (highTemp > 360) highTemp = 360;
+        if (highTemp > 360) highTemp = 360; // 120~360 Second
         if (highTemp < 120) highTemp = 120;
         
         // console.log('sun error: '+ err);
@@ -118,9 +117,7 @@ function crawlKimono() {
         str = hsinchu.results.Sun[0].sunset;
         sunsetHour = parseInt(str.substring(0,2))-1;    // Sprinkle before sunset
         sunsetMinute = parseInt(str.substring(3,5));
-        console.log('Daily default downcounter value: '+ highTemp);
-        console.log(sunriseHour+':'+sunriseMinute);
-        console.log(sunsetHour+':'+sunsetMinute);
+        console.log(new Date()+': '+highTemp+' C, '+rainAverage+' mm, '+sunriseHour+':'+sunriseMinute+', '+sunsetHour+':'+sunsetMinute);
     });
     setTimeout(crawlKimono, 60*minutes); // 1 hour period; no other state
 }
@@ -135,49 +132,71 @@ function checkSchedule() {
     } else setTimeout(checkSchedule, 20000); // 20 seconds cycle
 }
 
-function setCounter_Log() {             // 120~360 Second
+function setCounter_Log() {             // one-time state; only enter once
     if (accRain >= 1) {                 // check raining status
-        accRain -= 1;                   // reduce 1mm each time; it is raining outside
+        if (rainAverage < 1) {          // no more raining
+            accRain -= 1;               // reduce 1mm each time
+        }
         downCounter = 0;                // by pass Sprinkle
         setTimeout(checkSchedule, 1);   // change state
     } else {
         downCounter = Math.floor((highTemp * (1-accRain))); // set downCounter
         accRain = 0;
-        b.digitalWrite(RelayPin, 1);        // turn on motor
-        b.digitalWrite('USR0', 1);          // turns the LED ON
+        // b.digitalWrite(RelayPin, 1);    // turn on motor
+        // b.digitalWrite('USR0', 1);      // turns the LED ON
     
-        intervalObject = setInterval(function() { // broadcast.emit: server to "n clients"
-            downCounter--;
-            io.sockets.emit("downCounter", '{"downValue":"'+ downCounter +'"}');
-        }, 999); // one second interval count down; pass downCounter value to client
-        
-        // writer Log file out
-        
-        setTimeout(downCounting, 1); // state change
+        setTimeout(downCounting, 1);    // state change
     }
+    var d = new Date();                 // writer Log file out
+    console.log(d.toLocaleTimeString()+': sprinkle '+downCounting+' seconds');
 }
 
 function downCounting() {
     if (downCounter > 0) {
         setTimeout(downCounting, 999);
     } else {
-        b.digitalWrite(RelayPin, 0);    // turn off motor
-        b.digitalWrite('USR0', 0);      // turns the LED OFF
-        b.digitalWrite('USR1', 0);
-        b.digitalWrite('USR2', 0);
+        // b.digitalWrite(RelayPin, 0);    // turn off motor
+        // b.digitalWrite('USR0', 0);      // turns the LED OFF
+        // b.digitalWrite('USR1', 0);
+        // b.digitalWrite('USR2', 0);
         // console.log('\t\tGrundfos Hot Water Pump is the '+ logCounter +'th turn-off'); 
-        clearInterval(intervalObject);
+        // clearInterval(intervalObject);
         setTimeout(checkSchedule, 1);   // state change
     }
 }
 // .............................................................................
-function aliveSignal0() {               // Two States only
-    b.digitalWrite('USR3', 0);
-    setTimeout(aliveSignal1, 800);      // Toggle LED
-}
+var d = new Date();                 
+var logData = d.toLocaleTimeString()+': this is a test for log\n';
+logIt(logData);
 
-function aliveSignal1() {
-    b.digitalWrite('USR3', 1);
-    setTimeout(aliveSignal0, 200);      // Toggle LED
+function logIt(data) {                  // writer Log file out
+    fs.open('daily.txt', 'a+', function(err, fd) {
+        if (err) {
+            return console.error(err);
+        }   
+        // console.log("File opened successfully!"); 
+        fs.appendFile('daily.txt', data,  function(err) {
+           if (err) {
+                return console.error(err);
+           }
+        //   console.log("Data written successfully!");
+        });
+        fs.close(fd, function(err){
+            if (err) {
+                console.log(err);
+            } 
+            // console.log("File closed successfully.");
+        });
+    });
 }
+// .............................................................................
+// function aliveSignal0() {               // Two States only
+//     b.digitalWrite('USR3', 0);
+//     setTimeout(aliveSignal1, 800);      // Toggle LED
+// }
+
+// function aliveSignal1() {
+//     b.digitalWrite('USR3', 1);
+//     setTimeout(aliveSignal0, 200);      // Toggle LED
+// }
 // }
