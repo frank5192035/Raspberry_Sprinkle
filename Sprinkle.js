1408769f3c4f4c7e2d7a8df5dfe85c263f542661
Frank Hsiung <frank5192035@yahoo.com.tw>
2015-10-05 05:29:46 +0000

    change to node API https

--- Sprinkle.js
+++ Sprinkle.js
@@ -2,15 +2,13 @@
 //      by Frank Hsiung         Sep 20, 2015
 
 // Loading modules {
-// var http = require('http');
-// var path = require('path');
+var https = require('https');
 var fs = require('fs');
-var request = require('/usr/local/lib/node_modules/request');
-var gpio = require('/usr/local/lib/node_modules/onoff').Gpio;
+// var gpio = require('/usr/local/lib/node_modules/onoff').Gpio;
 // }----------------------------------------------------------------------------
 // set Pins {
-var RelayPin = new gpio(18, 'out');     // default Relay Pin
-RelayPin.writeSync(1);                  // turn off motor: active low
+// var RelayPin = new gpio(18, 'out');     // default Relay Pin
+// RelayPin.writeSync(1);                  // turn off motor: active low
 // }----------------------------------------------------------------------------
 // Global Variables and Constants {
 const hours = 60*60*1000;
@@ -27,51 +25,55 @@ var highTemp = 250;                     // Highest Temperature of the day *10
 setTimeout(checkSchedule, 1);           // Initialization for Main State
 setTimeout(crawlKimono, 1);             // Initialization for Kimono network spider
 // }----------------------------------------------------------------------------
-// }----------------------------------------------------------------------------
 // State Machine and Function Call {
 function crawlKimono() {
-    request("https://www.kimonolabs.com/api/6ucbaoiy?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn", 
-    function(err, response, body) {     // average rain of pass 24 hours
-        if (!err && response.statusCode == 200) {
-            var hsinchu = JSON.parse(body);
-            var tt = 0;
-            rainAverage = 0;
-            for (var i=0; i<4; i++) {
-                tt = parseFloat(hsinchu.results.Rain[i].mm);
-                if (isNaN(tt)) tt = 0;
-                rainAverage += tt;
+    https.get('https://www.kimonolabs.com/api/6ucbaoiy?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn', function(res) {
+        res.on('data', function(body) {
+            if (res.statusCode == 200) {
+                var hsinchu = JSON.parse(body);
+                var tt = 0;
+                rainAverage = 0;
+                for (var i=0; i<4; i++) {
+                    tt = parseFloat(hsinchu.results.Rain[i].mm);
+                    if (isNaN(tt)) tt = 0;
+                    rainAverage += tt;
+                }
+                rainAverage = Math.round(rainAverage*30) / 100; // 20% higher than average
+                if (rainAverage > 14) rainAverage = 14; // maximum setting for rainAverage = 1 week
+                if (rainAverage > accRain) accRain = rainAverage;
             }
-            rainAverage = Math.round(rainAverage*30) / 100; // 20% higher than average
-            if (rainAverage > 14) rainAverage = 14; // maximum setting for rainAverage = 1 week
-            if (rainAverage > accRain) accRain = rainAverage;
-        }
+        });
+    }).on('error', function(e) {
+        console.error(e);
     });
-    request("https://www.kimonolabs.com/api/78dba3cq?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn",
-    function(err, response, body) {     // Highest Temperature
-        var record;
-        if (!err && response.statusCode == 200) {
-            var hsinchu = JSON.parse(body);
-            var str = hsinchu.results.Sun[0].temp;
-            for (var i=2; i < str.length; i++) { 
-              if (str[i] == "~") {
-                  highTemp = parseInt(str.substring(i+2, str.length), 10)*10; // for down counting second
-              }
+    https.get('https://www.kimonolabs.com/api/78dba3cq?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn', function(res) {
+        res.on('data', function(body) {
+            if (res.statusCode == 200) {
+                var hsinchu = JSON.parse(body);
+                var str = hsinchu.results.Sun[0].temp;
+                for (var i=2; i < str.length; i++) { 
+                  if (str[i] == "~") {
+                      highTemp = parseInt(str.substring(i+2, str.length), 10)*10; // for down counting second
+                  }
+                }
+                if (highTemp > 360) highTemp = 360; // 120~360 Second
+                if (highTemp < 120) highTemp = 120;
+                
+                str = hsinchu.results.Sun[0].sunrise;
+                sunriseHour = parseInt(str.substring(0,2), 10)+1;   // Sprinkle one hour later
+                sunriseMinute = parseInt(str.substring(3,5), 10); 
+                str = hsinchu.results.Sun[0].sunset;
+                sunsetHour = parseInt(str.substring(0,2), 10)-1;    // Sprinkle before sunset
+                sunsetMinute = parseInt(str.substring(3,5), 10);
+                var record = new Date()+': '+highTemp+' C, '+rainAverage+' mm, '+sunriseHour+':'+sunriseMinute+', '+sunsetHour+':'+sunsetMinute+'\n';
+                logIt(record);
+            } else {
+                record = new Date()+': network error, bypass this hour\n';
+                logIt(record);
             }
-            if (highTemp > 360) highTemp = 360; // 120~360 Second
-            if (highTemp < 120) highTemp = 120;
-            
-            str = hsinchu.results.Sun[0].sunrise;
-            sunriseHour = parseInt(str.substring(0,2), 10)+1;   // Sprinkle one hour later
-            sunriseMinute = parseInt(str.substring(3,5), 10); 
-            str = hsinchu.results.Sun[0].sunset;
-            sunsetHour = parseInt(str.substring(0,2), 10)-1;    // Sprinkle before sunset
-            sunsetMinute = parseInt(str.substring(3,5), 10);
-            record = new Date()+': '+highTemp+' C, '+rainAverage+' mm, '+sunriseHour+':'+sunriseMinute+', '+sunsetHour+':'+sunsetMinute+'\n';
-            // logIt(record);
-        } else {
-            record = new Date()+': network error, bypass this hour\n';
-            // logIt(record);
-        }
+        });
+    }).on('error', function(e) {
+        console.error(e);
     });
     setTimeout(crawlKimono, 1*hours); // 1 hour period; no other state
 }
@@ -96,7 +98,7 @@ function setCounter_Log() {             // one-time state; only enter once
     } else {
         downCounter = Math.floor((highTemp * (1-accRain))); // set downCounter
         accRain = 0;
-        RelayPin.writeSync(0);          // turn on motor
+        // RelayPin.writeSync(0);          // turn on motor
         setTimeout(downCounting, 1);    // state change
     }
     var d = new Date();                 // writer Log file
@@ -109,7 +111,7 @@ function downCounting() {
     if (downCounter-- > 0) {
         setTimeout(downCounting, 1000);
     } else {
-        RelayPin.writeSync(1);          // turn off motor
+        // RelayPin.writeSync(1);          // turn off motor
         setTimeout(checkSchedule, 1*hours);   // state change; sprinkle finished
     }
 }
