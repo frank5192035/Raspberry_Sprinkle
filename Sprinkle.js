9a8cb96032c6a8f35ceb442cc807597db79a04bc
Frank Hsiung <frank5192035@yahoo.com.tw>
2015-10-23 05:21:47 +0000

    change to BBB platform

--- Sprinkle.js
+++ Sprinkle.js
@@ -1,14 +1,17 @@
 //      Main Program for Sprinkle
 //      by Frank Hsiung         Sep 20, 2015
+//      change to BBB since raspberry pi is unstable on running node
+//                              Oct 23, 2015
 
 // Loading modules {
-// var fs = require('fs');
+var fs = require('fs');
 var https = require('https');
-// var gpio = require('/usr/local/lib/node_modules/onoff').Gpio;
+var b = require('bonescript');
 // }----------------------------------------------------------------------------
 // set Pins {
-// var RelayPin = new gpio(18, 'out');     // default Relay Pin
-// RelayPin.writeSync(1);                  // turn off motor: active low
+var RelayPin = 'P9_12';                 // default Relay Pin
+b.pinMode(RelayPin, b.OUTPUT);          // Relay Pin for Turn ON/OFF Motor
+b.digitalWrite(RelayPin, 1);            // turn off motor; for low active relay
 // }----------------------------------------------------------------------------
 // Global Variables and Constants {
 const hours = 60*60*1000;
@@ -44,7 +47,8 @@ function crawlKimono() {
             }
         });
     }).on('error', function(e) {
-        console.error(e);
+        // console.error(e);
+        logIt(e);
     });
     https.get('https://www.kimonolabs.com/api/78dba3cq?apikey=lcE98jpR1ZSfMv1hY8eB9cTgEUAnhoTn', function(res) {
         res.on('data', function(body) {
@@ -66,14 +70,15 @@ function crawlKimono() {
                 sunsetHour = parseInt(str.substring(0,2), 10)-1;    // Sprinkle before sunset
                 sunsetMinute = parseInt(str.substring(3,5), 10);
                 var record = new Date()+': '+highTemp+' C, '+rainAverage+' mm, '+sunriseHour+':'+sunriseMinute+', '+sunsetHour+':'+sunsetMinute+'\n';
-                console.log(record);
+                logIt(record);
             } else {
                 record = new Date()+': network error, bypass this hour\n';
-                console.log(record);
+                logIt(record);
             }
         });
     }).on('error', function(e) {
-        console.error(e);
+        // console.error(e);
+        logIt(e);
     });
     setTimeout(crawlKimono, 1*hours); // 1 hour period; no other state
 }
@@ -86,7 +91,6 @@ function checkSchedule() {
         ((sunsetHour  == hour) && (sunsetMinute  == minute))) {
         setTimeout(setCounter_Log, 1);
     } else setTimeout(checkSchedule, 20000); // 20 seconds cycle
-    console.log(d+'\n');
 }
 
 function setCounter_Log() {             // one-time state; only enter once
@@ -96,39 +100,39 @@ function setCounter_Log() {             // one-time state; only enter once
     } else {
         downCounter = Math.floor((highTemp * (1-accRain))); // set downCounter
         accRain = 0;
-        // RelayPin.writeSync(0);          // turn on motor
+        b.digitalWrite(RelayPin, 0);    // turn on motor
         setTimeout(downCounting, 1);    // state change
     }
     var d = new Date();                 // writer Log file
     var record = d+': sprinkle '+downCounter+' second(accRain='+accRain+'）\n';
     // var record = d.toLocaleTimeString()+': sprinkle '+downCounter+' second(accRain='+accRain+'）\n';
-    console.log(record);
+    logIt(record);
 }
 
 function downCounting() {
     if (downCounter-- > 0) {
         setTimeout(downCounting, 1000);
     } else {
-        // RelayPin.writeSync(1);          // turn off motor
-        setTimeout(checkSchedule, 1*hours);   // state change; sprinkle finished
+        b.digitalWrite(RelayPin, 1);    // turn off motor
+        setTimeout(checkSchedule, 1*hours); // state change; sprinkle finished
     }
 }
 // .............................................................................
-// function logIt(data) {                  // writer Log file out
-//     fs.open('daily.txt', 'a+', function(err, fd) {
-//         if (err) {
-//             return console.error(err);
-//         }   
-//         fs.appendFile('daily.txt', data,  function(err) {
-//           if (err) {
-//                 return console.error(err);
-//           }
-//         });
-//         fs.close(fd, function(err){
-//             if (err) {
-//                 console.log(err);
-//             } 
-//         });
-//     });
-// }
+function logIt(data) {                  // writer Log file out
+    fs.open('daily.txt', 'a+', function(err, fd) {
+        if (err) {
+            return console.error(err);
+        }   
+        fs.appendFile('daily.txt', data,  function(err) {
+          if (err) {
+                return console.error(err);
+          }
+        });
+        fs.close(fd, function(err){
+            if (err) {
+                console.log(err);
+            } 
+        });
+    });
+}
 // }............................................................................
